7abf918914b5609d382d3996d2e95617492ed475
George Brunt <dexter@brunts.org>
2025-09-17 16:39:35 -0600

    reset board

--- shared/src/main/java/chess/BaseMovementRule.java
+++ shared/src/main/java/chess/BaseMovementRule.java
@@ -21,48 +21,48 @@ public abstract class BaseMovementRule implements MovementRule {
 
 
 
-    protected boolean isValidPosition(int row, int col) {
-        return row >= 1 && row <= 8 && col >= 1 && col <= 8;
-    }
-
-    protected boolean isEmptyOrCapturablePosition(ChessBoard board, ChessPosition newPosition) {
-        ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
-        return pieceAtNewPosition == null || pieceAtNewPosition.getTeamColor() != teamColor;
-    }
-
-
-    protected Collection<ChessMove> calculateStraightMoves(ChessBoard board, ChessPosition position) {
-        Collection<ChessMove> moves = new ArrayList<>();
-        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // right, down, left, up
-        for (int[] direction : directions) {
-            addMovesInDirection(moves, board, position, direction[0], direction[1]);
-        }
-        return moves;
-    }
-
-    protected Collection<ChessMove> calculateDiagonalMoves(ChessBoard board, ChessPosition position) {
-        Collection<ChessMove> moves = new ArrayList<>();
-        int[][] directions = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-        for (int[] direction : directions) {
-            addMovesInDirection(moves, board, position, direction[0], direction[1]);
-        }
-        return moves;
-    }
-
-    private void addMovesInDirection(Collection<ChessMove> moves, ChessBoard board, ChessPosition start, int rowDelta, int colDelta) {
-        int row = start.getRow();
-        int col = start.getColumn();
-        while (true) {
-            row += rowDelta;
-            col += colDelta;
-            ChessPosition newPosition = new ChessPosition(row, col);
-            if (!isValidPosition(row, col)) { break; }
-            if (isEmptyOrCapturablePosition(board, newPosition)) {
-                moves.add(new ChessMove(start, newPosition, null));
-                if (board.getPiece(newPosition) != null) { break; }// Stop if we captured a piece
-            } else {
-                break;
-            }
-        }
-    }
+//    protected boolean isValidPosition(int row, int col) {
+//        return row >= 1 && row <= 8 && col >= 1 && col <= 8;
+//    }
+//
+//    protected boolean isEmptyOrCapturablePosition(ChessBoard board, ChessPosition newPosition) {
+//        ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
+//        return pieceAtNewPosition == null || pieceAtNewPosition.getTeamColor() != teamColor;
+//    }
+//
+//
+//    protected Collection<ChessMove> calculateStraightMoves(ChessBoard board, ChessPosition position) {
+//        Collection<ChessMove> moves = new ArrayList<>();
+//        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // right, down, left, up
+//        for (int[] direction : directions) {
+//            addMovesInDirection(moves, board, position, direction[0], direction[1]);
+//        }
+//        return moves;
+//    }
+//
+//    protected Collection<ChessMove> calculateDiagonalMoves(ChessBoard board, ChessPosition position) {
+//        Collection<ChessMove> moves = new ArrayList<>();
+//        int[][] directions = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+//        for (int[] direction : directions) {
+//            addMovesInDirection(moves, board, position, direction[0], direction[1]);
+//        }
+//        return moves;
+//    }
+//
+//    private void addMovesInDirection(Collection<ChessMove> moves, ChessBoard board, ChessPosition start, int rowDelta, int colDelta) {
+//        int row = start.getRow();
+//        int col = start.getColumn();
+//        while (true) {
+//            row += rowDelta;
+//            col += colDelta;
+//            ChessPosition newPosition = new ChessPosition(row, col);
+//            if (!isValidPosition(row, col)) { break; }
+//            if (isEmptyOrCapturablePosition(board, newPosition)) {
+//                moves.add(new ChessMove(start, newPosition, null));
+//                if (board.getPiece(newPosition) != null) { break; }// Stop if we captured a piece
+//            } else {
+//                break;
+//            }
+//        }
+//    }
 }
--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -15,13 +15,12 @@ public class ChessBoard {
 
 
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        board[position.getRow()-1][position.getColumn()-1] = piece;
+        board[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     public ChessPiece getPiece(ChessPosition position) {
-        return  board[position.getRow()-1][position.getColumn()-1];
+        return board[position.getRow() - 1][position.getColumn() - 1];
     }
-
 
 
     /**
@@ -30,6 +29,38 @@ public class ChessBoard {
      */
     public void resetBoard() {
 
-        throw new RuntimeException("Not implemented");
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                board[i][j] = null;
+            }
+        }
+        // Set up white
+        addPiece(new ChessPosition(1, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(1, 2), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1, 3), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1, 4), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(1, 5), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(1, 6), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1, 7), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1, 8), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+
+        for (int i = 1; i <= 8; i++) {
+            addPiece(new ChessPosition(2, i),
+                    new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+        }
+        // Set up black
+        addPiece(new ChessPosition(8, 1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(8, 2), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8, 3), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8, 4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(8, 5), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(8, 6), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8, 7), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+
+        for (int i = 1; i <= 8; i++) {
+            addPiece(new ChessPosition(7, i),
+                    new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+        }
     }
 }
