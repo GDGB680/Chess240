9b5de9213e33c6b765d8330c13d568a59294f60c
George Brunt <dexter@brunts.org>
2025-11-14 14:17:00 -0700

    Update Main.java

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,21 @@
-import chess.*;
+package ui;
+
+import client.ServerFacade;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        String host = "localhost";
+        int port = 8080;
+
+        ServerFacade serverFacade = new ServerFacade(host, port);
+
+        while (true) {
+            PreloginUI preloginUI = new PreloginUI(serverFacade);
+            if (!preloginUI.run()) break; // User quit
+
+            // User is now logged in
+            PostloginUI postloginUI = new PostloginUI(serverFacade);
+            postloginUI.run(); // Returns when user logs out
+        }
     }
 }
