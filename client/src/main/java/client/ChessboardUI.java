40bd373961b374ab5aa1bd0e622c88b330a96d4c
George Brunt <dexter@brunts.org>
2025-11-18 15:58:15 -0700

    Update ChessboardUI.java

--- client/src/main/java/client/ChessboardUI.java
+++ client/src/main/java/client/ChessboardUI.java
@@ -8,24 +8,5 @@ public class ChessboardUI {
     private static final String[] RANKS = {"8", "7", "6", "5", "4", "3", "2", "1"};
 
 
-    private static String getSquareColor(int row, int col) {
-        boolean isLight = (row + col) % 2 == 1;
-        return isLight ? EscapeSequences.SET_BG_COLOR_LIGHT_GREY : EscapeSequences.SET_BG_COLOR_DARK_GREY;
-    }
 
-    private static String getPieceString(ChessPiece piece) {
-        if (piece == null) {return EscapeSequences.EMPTY;}
-
-        boolean isWhite = piece.getTeamColor() == ChessGame.TeamColor.WHITE;
-
-        return switch (piece.getPieceType()) {
-            case KING -> isWhite ? EscapeSequences.WHITE_KING : EscapeSequences.BLACK_KING;
-            case QUEEN -> isWhite ? EscapeSequences.WHITE_QUEEN : EscapeSequences.BLACK_QUEEN;
-            case ROOK -> isWhite ? EscapeSequences.WHITE_ROOK : EscapeSequences.BLACK_ROOK;
-            case BISHOP -> isWhite ? EscapeSequences.WHITE_BISHOP : EscapeSequences.BLACK_BISHOP;
-            case KNIGHT -> isWhite ? EscapeSequences.WHITE_KNIGHT : EscapeSequences.BLACK_KNIGHT;
-            case PAWN -> isWhite ? EscapeSequences.WHITE_PAWN : EscapeSequences.BLACK_PAWN;
-            default -> EscapeSequences.EMPTY;
-        };
-    }
 }
