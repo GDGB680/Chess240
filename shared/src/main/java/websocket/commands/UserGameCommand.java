de60863e3d6b169de589afcf63d7e8e1ee074a7f
George Brunt <dexter@brunts.org>
2025-12-04 17:42:18 -0700

    added movement functionality to UserGameCommand

--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -1,5 +1,6 @@
 package websocket.commands;
 
+import chess.ChessMove;
 import java.util.Objects;
 
 /**
@@ -16,10 +17,13 @@ public class UserGameCommand {
 
     private final Integer gameID;
 
-    public UserGameCommand(CommandType commandType, String authToken, Integer gameID) {
+    private final ChessMove move;
+
+    public UserGameCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
         this.commandType = commandType;
         this.authToken = authToken;
         this.gameID = gameID;
+        this.move = move;
     }
 
     public enum CommandType {
@@ -41,6 +45,8 @@ public class UserGameCommand {
         return gameID;
     }
 
+    public ChessMove getMove() { return move; }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -51,11 +57,12 @@ public class UserGameCommand {
         }
         return getCommandType() == that.getCommandType() &&
                 Objects.equals(getAuthToken(), that.getAuthToken()) &&
-                Objects.equals(getGameID(), that.getGameID());
+                Objects.equals(getGameID(), that.getGameID()) &&
+                Objects.equals(getMove(), that.getMove());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getCommandType(), getAuthToken(), getGameID());
+        return Objects.hash(getCommandType(), getAuthToken(), getGameID(), getMove());
     }
 }
