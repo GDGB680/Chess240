14d6c492a1329646a14a043af890f7a3228e77d5
George Brunt <dexter@brunts.org>
2025-12-05 17:13:16 -0700

    Update WebSocketConnection.java

--- client/src/main/java/client/WebSocketConnection.java
+++ client/src/main/java/client/WebSocketConnection.java
@@ -9,14 +9,14 @@ import websocket.messages.ServerMessage;
 
 import java.net.URI;
 import java.util.concurrent.CountDownLatch;
-
+import java.util.concurrent.TimeUnit;
 
 @WebSocket
 public class WebSocketConnection {
     private Session session;
     private final Gson gson = new Gson();
     private final WebSocketMessageListener listener;
-    private static final CountDownLatch connectLatch = new CountDownLatch(1);
+    private final CountDownLatch connectLatch;
 
     public interface WebSocketMessageListener {
         void onServerMessage(ServerMessage message);
@@ -24,12 +24,45 @@ public class WebSocketConnection {
 
     public WebSocketConnection(String serverUrl, WebSocketMessageListener listener) throws Exception {
         this.listener = listener;
+        this.connectLatch = new CountDownLatch(1);
 
         WebSocketClient client = new WebSocketClient();
         client.start();
 
         URI uri = new URI(serverUrl.replace("http", "ws") + "/ws");
         client.connect(this, uri);
-        connectLatch.await();
+
+        if (!connectLatch.await(5, TimeUnit.SECONDS)) {
+            throw new Exception("WebSocket connection timeout");
+        }
+    }
+
+    @OnWebSocketConnect
+    public void onConnect(Session session) {
+        this.session = session;
+        connectLatch.countDown();
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(String message) {
+        ServerMessage serverMessage = gson.fromJson(message, ServerMessage.class);
+        listener.onServerMessage(serverMessage);
+    }
+
+    @OnWebSocketClose
+    public void onClose(int statusCode, String reason) {
+        System.out.println("WebSocket closed: " + reason);
+    }
+
+    public void send(UserGameCommand command) throws Exception {
+        if (session != null && session.isOpen()) {
+            session.getRemote().sendString(gson.toJson(command));
+        }
+    }
+
+    public void close() throws Exception {
+        if (session != null) {
+            session.close();
+        }
     }
 }
